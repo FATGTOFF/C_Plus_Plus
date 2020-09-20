e707342b8999734ea178488cde65617139664423
Carlos Vazquez Figueroa <carlos.vazquezfigueroa@gmail.com>
2020-09-20 03:29:31 -0400

    Update main.cpp
    Modified the code to delete duplication.

--- EncodeArray/EncodeArray/main.cpp
+++ EncodeArray/EncodeArray/main.cpp
@@ -25,8 +25,6 @@
 #include <cstdlib> // For abs function.
 
 int* encodeArray(int);
-int* positiveNum(int, int&);
-int* negativeNum(int, int&);
 int arraySize(int);
 void printArray(int*, int);
 
@@ -37,78 +35,60 @@ int main()
 	std::cout << "Enter an integer to encode it: ";
 	std::cin >> num;
 
-	int *pArray {encodeArray(num)};
+	const int size{ arraySize(num) };
+	int* pArray{ encodeArray(num) };
+
+	printArray(pArray, size);
+
+	//Or
+
+	//std::cout << "{ ";
+	//for (int count = 0; count < size; count++)
+	//	std::cout << pArray[count] << " ";
+
+	//std::cout << "}\n";
 
 	// Deallocate the memory that was allocated under
 	// the positiveNum function or negativeNum function (depending
 	// on the user's input of the integer number. The positiveNum or
 	// negativeNum function will be called from the encodeArray function.
-	delete [] pArray;
+	delete[] pArray;
 	pArray = nullptr;
 
 }
 
 int* encodeArray(int num)
 {
-	int *arrNum {nullptr};
-	int size{};
-
-	if (num > 0)
-	{ 
-		arrNum = positiveNum(num, size);
-	}
-	// If the number is negative, then go to the negativeNum function,
-	// which will allocate one more element in the array to hold the number
-	// -1 into the first element in the array.
-	else if (num < 0)
-	{
-		arrNum = negativeNum(std::abs(num), size); // Use the abs function to change from neg to pos number.
-	}
-
-	printArray(arrNum, size);
-
-	return arrNum; // Returns the pointer to the dynamically allocated array.
-}
-
-int arraySize(int num)
-{
-	// Make one pass through the digits of the number 
-	// to compute the size of the encoded array.
-	int countNumDigits{ 0 };
-	int runningTotal{ 0 };
-	int absoluteNum {std::abs(num)};
-
-	while (absoluteNum > 0)
-	{
-		int digit = absoluteNum % 10; // Use the modulus operator to determine a single digit.
-		absoluteNum /= 10; // Obtain the other digits to the left of the single digit.
-		runningTotal += digit; // Add the single digits (i.e. 1234 would equal 10).
-		countNumDigits++; // Keep the count of the number of digits (i.e. 123 would count 3).
-	}
-	const int size{ runningTotal + countNumDigits }; // Determine the size of the array.
-
-	return size;
-}
-
-int* positiveNum(int num, int& size)
-{
-	 size = arraySize(num); // Save the size of the array.
+	const int size{ arraySize(num) };
+	int absoluteValue{ std::abs(num) };
 
 	// Dynamically allocate the array with the size
 	// determined by the function arraySize.
-	int *arrNum = nullptr;
+	int* arrNum = nullptr;
 	arrNum = new int[size];
 
-	// Set index of the encoded array to 1.
-	for (int count = 0; count < size; count++)
-		arrNum[count] = 1;
+	if (num < 0) // If the number is negative, an extra element is allocated
+	{
+		arrNum[0] = -1; // The first element must be -1 for negative numbers.
+
+		// Set index of the encoded array from the second element to 1.
+		for (int count = 1; count < size; count++)
+			arrNum[count] = 1;
+	}
+
+	else
+	{
+		// Set index of the encoded array to 1.
+		for (int count = 0; count < size; count++)
+			arrNum[count] = 1;
+	}
 
 	int index{ size - 1 }; // Save the last index from the array.
 
-	while (num > 0)
+	while (absoluteValue > 0)
 	{
-		int digit = num % 10; // Use the modulus operator to determine a single digit.
-		num /= 10; // Obtain the other digits to the left of the single digit.
+		int digit = absoluteValue % 10; // Use the modulus operator to determine a single digit.
+		absoluteValue /= 10; // Obtain the other digits to the left of the single digit.
 
 		// Get the difference between the index and the single
 		// digit extracted to place the 0s (i.e. if the digit is 2,
@@ -139,47 +119,38 @@ int* positiveNum(int num, int& size)
 	}
 
 	return arrNum; // Returns the pointer to the dynamically allocated array.
+
 }
 
-int* negativeNum(int num, int& size)
+int arraySize(int num)
 {
-	size = arraySize(num) + 1; // Additional element to define the first element as -1
-	int *arrNum = nullptr;
-	arrNum = new int[size];
+	// Make one pass through the digits of the number 
+	// to compute the size of the encoded array.
+	int countNumDigits{ 0 };
+	int runningTotal{ 0 };
+	int absoluteNum{ std::abs(num) };
+	int size{ 0 };
 
-	arrNum[0] = -1; // The first element must be -1 for negative numbers.
-
-	// Set index of the encoded array from the second element to 1.
-	for (int count = 1; count < size; count++)
-		arrNum[count] = 1;
-
-	int index{ size - 1 }; // Save the last index from the array.
-
-	while (num > 0)
+	while (absoluteNum > 0)
 	{
-		int digit = num % 10;
-		num /= 10;
-		int diffMaxandMinElem{ index - digit };
-
-		for (int count = index - 1; count >= diffMaxandMinElem || digit == 0; count--)
-		{
-			if (digit == 0)
-			{
-				index = count;
-				break;
-			}
-
-			else
-			{
-				arrNum[count] = 0;
-				index = count - 1;
-			}
-
-		}
-
+		int digit = absoluteNum % 10; // Use the modulus operator to determine a single digit.
+		absoluteNum /= 10; // Obtain the other digits to the left of the single digit.
+		runningTotal += digit; // Add the single digits (i.e. 1234 would equal 10).
+		countNumDigits++; // Keep the count of the number of digits (i.e. 123 would count 3).
 	}
 
-	return arrNum; // Returns the pointer to the dynamically allocated array.
+	// If the num inputted by the user is positive
+	if (num > 0)
+	{
+		size = runningTotal + countNumDigits;
+	}
+
+	else// An extra array element to place the -1 as the first element.
+	{
+		size = (runningTotal + countNumDigits) + 1;
+	}
+
+	return size;
 }
 
 void printArray(int* arrNum, int size)
@@ -188,5 +159,5 @@ void printArray(int* arrNum, int size)
 	for (int count = 0; count < size; count++)
 		std::cout << arrNum[count] << " ";
 
-	std::cout <<"}"; // Backspaces to eliminate the last comma.
+	std::cout << "}\n";
 }
