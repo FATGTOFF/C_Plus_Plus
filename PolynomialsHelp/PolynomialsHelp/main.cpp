eb722a72a26ca025314aef1e969cb11b6659c945
Carlos Vazquez Figueroa <carlos.vazquezfigueroa@gmail.com>
2022-10-23 00:38:39 -0400

    Add files via upload

--- PolynomialsHelp/PolynomialsHelp/main.cpp
+++ PolynomialsHelp/PolynomialsHelp/main.cpp
@@ -11,7 +11,7 @@
 */
 struct Polynomial
 {
-	static const int Max_Elements{20};
+	static const int Max_Elements{ 20 };
 	int polyOrder; // Integer that represents the polynomial order.
 	double xValue; // Double that holds the x value.
 	double coefficients[Max_Elements]; // Array of doubles that represents the polynomial coefficients.
@@ -24,7 +24,7 @@ void printPoly(Polynomial);
 
 int main()
 {
-	Polynomial polyValues{readPoly()};
+	Polynomial polyValues{ readPoly() };
 	printPoly(polyValues);
 
 	return 0;
@@ -64,13 +64,13 @@ Polynomial readPoly()
 	This evalPoly function takes the Polynomial data structure
 	as a parameter to calculate the polynomial to enable to return
 	the value as a double to the calling function. It initializes a
-	for loop to iterate thru the coefficients based on the input of the 
+	for loop to iterate thru the coefficients based on the input of the
 	user of the number of coefficients the user wishes to input.
 */
 double evalPoly(Polynomial p)
 {
 	double result{};
-	int countExpOrder{p.polyOrder}; // This variable is to maintain the exponential count to decrement each coefficient. 
+	int countExpOrder{ p.polyOrder }; // This variable is to maintain the exponential count to decrement each coefficient. 
 	for (int count = 0; count <= p.polyOrder; count++)
 	{
 		result += p.coefficients[count] * pow(p.xValue, countExpOrder);
@@ -88,58 +88,35 @@ double evalPoly(Polynomial p)
 */
 void printPoly(Polynomial p)
 {
-	//std::cout << std::noshowpos;
-	double result{evalPoly(p)}; // Calls the function to obtain the final value of the polynomial calculation.
-	int counterExpOrder{p.polyOrder}; // Initialize the highest exponent count to decrement as each coefficient iteration.
+	double result{ evalPoly(p) }; // Calls the function to obtain the final value of the polynomial calculation.
+	int counterExpOrder{ p.polyOrder }; // Initialize the highest exponent count to decrement as each coefficient iteration.
 
-	std::cout << "f (x) = ";
+	std::cout << "f(x) = ";
 	std::cout << *p.coefficients << " x^" << p.polyOrder; // Display the first coefficient prior to begin looping.
 	counterExpOrder--;
 	for (int count = 1; count <= p.polyOrder; count++)
 	{
-		//std::cout << std::noshowpos << " ";
-		if (p.coefficients[count] /*>*/!= 0) // As long as the coefficient is higher than 0, continue to print to the output.
+		if (p.coefficients[count] != 0) // As long as the coefficient is higher than 0, continue to print to the output.
 		{
 			if (counterExpOrder > 1)
 			{
-				std::cout << "+ " << p.coefficients[count]; 
+				std::cout << "+ " << p.coefficients[count];
 				std::cout << " x^" << counterExpOrder /*<<" + "*/; /*<<*//* std::setw(2) << std::showpos*/ /*<< " "std::showpos <<*/ /*" "*//*" + "*///;
 				--counterExpOrder;
-				//std::cout << std::noshowpoint;
 			}
 
 			else if (counterExpOrder == 1) // If the exponent is 1, then drop the ^ 1 due to is not required to display x ^ 1.
 			{
-				std::cout << "+ " << p.coefficients[count]; 
+				std::cout << "+ " << p.coefficients[count];
 				std::cout << " x ";
 				--counterExpOrder;
 			}
 			else if (counterExpOrder == 0) // If the exponent is 0, then drop the x since x ^ 0 = 1.
 			{
-				std::cout << "+ " /*<< std::showpos*/ << p.coefficients[count];
+				std::cout << "+ " << p.coefficients[count];
 			}
 
 		}
-		//else if (p.coefficients[count] < 0)
-		//{
-		//	if (counterExpOrder > 1)
-		//	{
-		//		std::cout << " - " << p.coefficients[count];
-		//		std::cout << " x^" << counterExpOrder /*<<*/ /*std::showpos <<*/ /*" "*//*" + "*/;
-		//		--counterExpOrder;
-		//	}
-
-		//	else if (counterExpOrder == 1) // If the exponent is 1, then drop the ^ 1 due to is not required to display x ^ 1.
-		//	{
-		//		std::cout << " - " << p.coefficients[count];
-		//		std::cout << " x ";
-		//		--counterExpOrder;
-		//	}
-		//	else if (counterExpOrder == 0) // If the exponent is 0, then drop the x since x ^ 0 = 1.
-		//	{
-		//		std::cout << "- " /*<< std::showpos*/ << p.coefficients[count];
-		//	}
-		//}
 
 		else // If the coefficient is 0, then decrement the exponent and iterate to the next coefficient.
 		{
@@ -150,5 +127,5 @@ void printPoly(Polynomial p)
 
 	}
 
-	std::cout << "\nf (x = " << p.xValue << ") = " << result;
+	std::cout << "\nf(x = " << p.xValue << ") = " << result;
 }
