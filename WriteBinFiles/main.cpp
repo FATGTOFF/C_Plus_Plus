45d6bd350b0ad03935093c2d4a21223de584a90b
Carlos Vazquez Figueroa <carlos.vazquezfigueroa@gmail.com>
2024-09-30 09:57:44 -0400

    Add files via upload
    Updated to be more C++ by using the string object.

--- WriteBinFiles/main.cpp
+++ WriteBinFiles/main.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
+#include <string>
 #include <fstream>
 
 
 int main()
 {
 
-    const char* name = "This is a test";
-    std::fstream scoreFile{};
-    scoreFile.open("BinaryFile.bin", std::ios::out | std::ios::binary);
-    scoreFile.write(reinterpret_cast<char*>(&name), sizeof(name));
+   std::string name = "This is a test";
+   std::fstream binFile{};
+   binFile.open("BinaryFile.bin", std::ios::out | std::ios::binary);
+   binFile.write(name.c_str(), name.size());
 
-    scoreFile.close();
+   binFile.close();
 
 }
