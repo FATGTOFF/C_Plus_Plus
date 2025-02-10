54d56ea2898afe2ae9932fff5b26168273003bac
Carlos Vazquez Figueroa <carlos.vazquezfigueroa@gmail.com>
2025-02-09 21:51:37 -0500

    Add files via upload

--- Hangman_ver.3.0/Logger/Headers/SharedMemory.h
+++ Hangman_ver.3.0/Logger/Headers/SharedMemory.h
@@ -9,7 +9,72 @@
 		(DateTime::TimeStamp::LOG_DAY_MON_YR_HR_MIN_SEC_MS) \
 		<< "Error Code: " << GetLastError() << " - "
 
-static Logger logger_shared{};
+#define LOG_TO_FILE_DAY_MON_YR_HR_MIN_SEC                   \
+		logger->fileOutPut                                  \
+		(DateTime::TimeStamp::LOG_DAY_MON_YR_HR_MIN_SEC)    \
+		<< "Error Code: " << GetLastError() << " - "
+
+#define LOG_TO_FILE_DAY_MON_YR_HR_MIN                       \
+		logger->fileOutPut                                  \
+		(DateTime::TimeStamp::LOG_DAY_MON_YR_HR_MIN)        \
+		<< "Error Code: " << GetLastError() << " - "
+
+#define LOG_TO_FILE_DAY_MON_YR                              \
+		logger->fileOutPut                                  \
+		(DateTime::TimeStamp::LOG_DAY_MON_YR)               \
+		<< "Error Code: " << GetLastError() << " - "
+
+#define LOG_TO_FILE_HR_MIN_SEC_MS                           \
+		logger->fileOutPut                                  \
+		(DateTime::TimeStamp::LOG_HR_MIN_SEC_MS)            \
+		<< "Error Code: " << GetLastError() << " - "
+
+#define LOG_TO_FILE_HR_MIN_SEC                              \
+		logger->fileOutPut                                  \
+		(DateTime::TimeStamp::LOG_HR_MIN_SEC)               \
+		<< "Error Code: " << GetLastError() << " - "
+
+#define LOG_TO_FILE_HR_MIN                                  \
+		logger->fileOutPut                                  \
+		(DateTime::TimeStamp::LOG_HR_MIN)                   \
+		<< "Error Code: " << GetLastError() << " - "
+
+#define LOG_TO_FILE_DAY                                     \
+		logger->fileOutPut                                  \
+		(DateTime::TimeStamp::LOG_DAY)                      \
+		<< "Error Code: " << GetLastError() << " - "
+
+#define LOG_TO_FILE_MONTH                                   \
+		logger->fileOutPut                                  \
+		(DateTime::TimeStamp::LOG_MONTH)                    \
+		<< "Error Code: " << GetLastError() << " - "
+
+#define LOG_TO_FILE_YEAR                                    \
+		logger->fileOutPut                                  \
+		(DateTime::TimeStamp::LOG_YEAR)                     \
+		<< "Error Code: " << GetLastError() << " - "
+
+#define LOG_TO_FILE_HRS                                     \
+		logger->fileOutPut                                  \
+		(DateTime::TimeStamp::LOG_HRS)                      \
+		<< "Error Code: " << GetLastError() << " - "
+
+#define LOG_TO_FILE_MINS                                    \
+		logger->fileOutPut                                  \
+		(DateTime::TimeStamp::LOG_MINS)                     \
+		<< "Error Code: " << GetLastError() << " - "
+
+#define LOG_TO_FILE_SECS                                    \
+		logger->fileOutPut                                  \
+		(DateTime::TimeStamp::LOG_SECS)                     \
+		<< "Error Code: " << GetLastError() << " - "
+
+#define LOG_TO_FILE_MS                                      \
+		logger->fileOutPut                                  \
+		(DateTime::TimeStamp::LOG_MS)                       \
+		<< "Error Code: " << GetLastError() << " - "
+
+const static Logger logger_shared{};
 
 struct SharedMemory
 {
