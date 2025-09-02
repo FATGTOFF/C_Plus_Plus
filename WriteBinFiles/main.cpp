076811a158ac76fa1a87b838a525ef7325b8058f
Carlos Vazquez Figueroa <carlos.vazquezfigueroa@gmail.com>
2025-09-02 02:12:23 -0400

    Update main.cpp

--- WriteBinFiles/main.cpp
+++ WriteBinFiles/main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-#include <string>
 #include <fstream>
 
 
 int main()
 {
 
-	std::string name = "This is a test";
+	const char name[] = "This is a test";
 	std::fstream binFile{};
 	binFile.open("BinaryFile.bin", std::ios::out | std::ios::binary);
 	binFile.write(reinterpret_cast<const char*>(&name), sizeof(name));
